f02902dd7a24f499e7daf4972e9a1fd3204a09d9
asyl0512 <asyl051297@gmail.com>
2024-04-08 01:15:47 +0000

    changed bucket prefix

--- homework2/main.tf
+++ homework2/main.tf
@@ -1,23 +1,23 @@
-# resource "aws_key_pair" "Bastion" {
-#   key_name   = "Bastion-key"
-#   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDLuX68O+bUUWYJmgC2bzdIc9CMChiTQ2lRrK+bRtWEdF9Xv6lyUvMrhvcb7eUQlzS2ez0iGh8vktPscTmYop39Lsi4Sa7Dg/xBIrUJ8fugBv1um6pWNFqLObCFHurNsr1kOehbwk/rnjNck4HauRsWdgvX4uRd1f3vLQpnwWKrLkto0dOowR7OV6AtvoucdmY2MsUqAKFDaxEA9w55ZegC9wVKcbAvifNSpzYDDSUEd75/wGdccjk5LYwdRQYbbtdIT+/o/b01xGSPBreEvtzA7fn1xGoZZHw2kqiAzOjCyNvKxL4uPYSHgOJ328omRp3EutMrUIog57myUMFcV2o/XMifj6nDIeLBYar03M6qQBS4g+VXoDanxmmk1OZyqPApHAVnMWIkO8qut3wY/Wi+c07ZPO7nM5RRWEMrQ8a1ybI6ZB2P6XA3m06DyaTF28LQuE7j5s8ZJB2vQP+iNQyBasd8+4YSLiwrRhdIUatJQgEna0hsfGRENtPKBEEunmE= user@Users-Air.lan"
-# }
+resource "aws_key_pair" "Bastion" {
+  key_name   = "Bastion-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
 
-# resource "aws_s3_bucket" "bucket1" {
-#   bucket = "kaizen-asyl"
-# }
+resource "aws_s3_bucket" "kaizen_yourname" {
+  bucket = "kaizen-asyl"
+}
 
-# resource "aws_s3_bucket" "bucket2" {
-#   bucket = "kaizen-asel"
-# }
+resource "aws_s3_bucket" "kaizen_with_prefix" {
+  bucket_prefix = "kaizen-"
+}
 
-# resource "aws_s3_bucket" "bucket3" {
-#   bucket = "kaizen-serik"
-# }
+resource "aws_s3_bucket" "bucket1" {
+  bucket = "kaizen-serik"
+}
 
-# resource "aws_s3_bucket" "bucket4" {
-#   bucket = "kaizen-berik"
-# }
+resource "aws_s3_bucket" "bucket2" {
+  bucket = "kaizen-berik"
+}
 
 # terraform import aws_s3_bucket.bucket3 kaizen-serik
 # terraform import aws_s3_bucket.bucket4 kaizen-berik
