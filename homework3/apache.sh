d2e7dc02a7bae9211c1669d38d2d5fc51b763250
asyl0512 <asyl051297@gmail.com>
2024-04-09 17:33:54 +0000

    updated script

--- homework3/apache.sh
+++ homework3/apache.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install apache2 -y
-sudo systemctl start apache2
-sudo systemctl enable apache2
+sudo yum update -y
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
 echo "<html><body><h1>Hello, World!</h1></body></html>" | sudo tee /var/www/html/hello.html
-sudo systemctl restart apache2
-
-
-
+sudo systemctl restart httpd
